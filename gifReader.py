df234e07e3d4232f3b89cfea0b815557f25f825c
DIAMOND <XXMOHAMMADXxXx754@gmail.com>
2025-06-21 14:01:02 +0300

    Added running skulls through arguments

--- gifReader.py
+++ gifReader.py
@@ -1,7 +1,7 @@
 import time
 import os
 from termcolor import colored
-
+import argparse
 
 def read_file(file_path):
     try:
@@ -13,44 +13,33 @@ def read_file(file_path):
     except IOError:
         print(f"An error occurred while reading the file {file_path}.")
 
-
 def firstSkull():
-
     firstSkullMainPath = 'gifs/1/frames/ASCII/'
-
     for x in range(17):
         time.sleep(0.06)
         os.system("clear")
         currentFilePath = firstSkullMainPath + str(x) + ".txt"
         read_file(currentFilePath)
 
-
 def secondSkull():
-
     secondSkullMainPath = 'gifs/2/frames/ASCII/'
-
     for x in range(10):
         time.sleep(0.1)
         os.system("clear")
         currentFilePath = secondSkullMainPath + str(x) + ".txt"
         read_file(currentFilePath)
 
-
 def outro():
-
     outroMainPath = 'gifs/outro/frames/ASCII/'
     shoudlEmpty = False
-
     for y in range(2):
         for x in range(6):
             if shoudlEmpty:
-
                 time.sleep(0.1)
                 os.system("clear")
                 shoudlEmpty = False
                 currentFilePath = outroMainPath + "e.txt"
                 read_file(currentFilePath)
-
             else:
                 time.sleep(0.05)
                 os.system("clear")
@@ -58,50 +47,47 @@ def outro():
                 read_file(currentFilePath)
                 shoudlEmpty = True
 
-
 def end():
-
     def fix(firstTime):
-
         if firstTime:
             for x in range(5):
                 print("\n")
-
         print("\t\t", end="")
         print("     ", end="")
-
+    
     def blinkJoinUs():
-
         os.system("clear")
-
         firstTime = True
         text = "JOIN US"
-
         for x in range(2):
-
             if firstTime:
                 fix(1)
                 firstTime = False
             else:
                 fix(0)
-
             print(colored(text, 'white', 'on_green'), end='\r')
             time.sleep(0.5)
-
             if firstTime:
                 fix(1)
                 firstTime = False
             else:
                 fix(0)
-
             print(text, end='\r')
             time.sleep(0.5)
-
     blinkJoinUs()
     os.system("clear")
 
 
-firstSkull()
-secondSkull()
-outro()
-end()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run skull animations")
+    parser.add_argument("steps", nargs='+', type=int, choices=[1, 2, 3],
+                        help="Specify which steps to run: 1=firstSkull, 2=secondSkull, 3=outro")
+
+    args = parser.parse_args()
+    if 1 in args.steps:
+        firstSkull()
+    if 2 in args.steps:
+        secondSkull()
+    if 3 in args.steps:
+        outro()
+    end()
